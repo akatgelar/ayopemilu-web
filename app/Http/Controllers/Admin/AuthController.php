69572c3afb4f8262cbb2a2f5850ef87cfda94ca6
akatgelar <akatgelar@gmail.com>
2024-06-30 18:10:34 +0700

    feat: admin login

---
+++ app/Http/Controllers/Admin/AuthController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+    }
+
+    public function index()
+    {
+        return $this->login();
+    }
+
+    public function logout()
+    {
+        $has_session = \App\Helpers\AppHelper::instance()->removeSession();
+        return redirect()->route('admin.login');
+    }
+
+    public function login()
+    {
+        $has_session = \App\Helpers\AppHelper::instance()->checkSession();
+
+        if($has_session) {
+            return redirect()->route('admin.dashboard');
+        } else {
+            $data = [];
+            $data['setting'] = \App\Helpers\AppHelper::instance()->requestApiSetting();
+            $data['message'] = session('message', null);
+            return view('admin.auth.login', $data);
+        }
+
+    }
+
+    public function action_login(Request $request)
+    {
+        $data = [];
+        $data['setting'] = \App\Helpers\AppHelper::instance()->requestApiSetting();
+
+        $payload = $request->all();
+        $result = \App\Helpers\AppHelper::instance()->requestApiLogin('/api/login', $payload);
+        print_r($result);
+        $data['result'] = $result[0];
+        $data['message'] = $result[1];
+
+        print_r($data['result']['data']);
+        print_r($data['result']['auth']['token']);
+
+        if($data['result']) {
+            \App\Helpers\AppHelper::instance()->setSession($data['result']['data'], $data['result']['auth']['token']);
+            return redirect()->route('admin.dashboard');
+        } else {
+            session()->flash('message', $data['message']);
+            return redirect()->route('admin.login');
+        }
+    }
+
+    public function forgot()
+    {
+        $data = [];
+        $data['setting'] = \App\Helpers\AppHelper::instance()->requestApiSetting();
+        return view('admin.auth.forgot', $data);
+    }
+
+    public function error()
+    {
+        $data = [];
+        $data['setting'] = \App\Helpers\AppHelper::instance()->requestApiSetting();
+        return view('admin.auth.error', $data);
+    }
+}
