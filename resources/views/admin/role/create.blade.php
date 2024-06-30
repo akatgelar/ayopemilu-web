10b4b20ba823e3404a211244cb6eed47987ce0e3
akatgelar <akatgelar@gmail.com>
2024-06-30 18:44:35 +0700

    feat: admin role

---
+++ resources/views/admin/role/create.blade.php
@@ -0,0 +1,112 @@
+@extends('admin.layouts.app')
+
+@section('content')
+    <div class="conatiner-fluid content-inner mt-n5 py-0" style="margin-top: 100px !important;">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="flex-wrap d-flex justify-content-between align-items-center">
+                            <div>
+                                <div class="header-title">
+                                    <h2 class="card-title">Role</h2>
+                                    <p>Create data</p>
+                                </div>
+                            </div>
+                            <div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <form method="POST" class="needs-validation" id="form-data" name="form-data" novalidate>
+                            {{ csrf_field() }}
+                            <div class="form-group">
+                                <label class="form-label" for="name">Name </label>
+                                <input class="form-control" type="text" id="name" name="name" value="" placeholder="Enter Name" required>
+                            </div>
+                            <div class="form-group">
+                                <label class="form-label" for="notes">Notes</label>
+                                <textarea class="form-control" id="notes" name="notes" rows="3"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <div class="form-check form-switch">
+                                    <input class="form-check-input" type="checkbox" id="status" name="status">
+                                    <label class="form-check-label" for="status">Active Status</label>
+                                </div>
+                            </div>
+                            <br><br>
+                            <button type="submit" class="btn btn-success">Submit</button>
+                            <a href="{{ URL::previous() }}" class="btn btn-danger">Cancel</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+
+        // handle post
+        $('#form-data').submit(false);
+        $("#form-data").submit( function () {
+
+            if($(this).valid()) {
+                var form = $("#form-data").serializeArray();
+                var formdata = {};
+                $.map(form, function(n, i){
+                    formdata[n['name']] = n['value'];
+                });
+                if ('status' in formdata) {
+                    if (formdata['status'] == 'on') {
+                        formdata['status'] = '1';
+                    } else {
+                        formdata['status'] = '0';
+                    }
+                } else {
+                    formdata['status'] = '0';
+                }
+
+                $.ajaxSetup({
+                    headers:{
+                        'Authorization': "Bearer {{$session_token}}"
+                    }
+                });
+                $.ajax({
+                    url: '/api/role',
+                    type: "POST",
+                    data: JSON.stringify(formdata),
+                    contentType: "application/json; charset=utf-8",
+                    dataType: "json",
+                    processData: false,
+                    success: function (result) {
+                        if(result['status'] == true) {
+                            Swal.fire({
+                                icon: "success",
+                                title: "Success",
+                                text: result['message'],
+                                confirmButtonColor: '#3A57E8',
+                            }).then((result) => {
+                                window.location.replace("{{ url('/admin/role') }}");
+                            });
+                        } else {
+                            Swal.fire({
+                                icon: "error",
+                                title: "Oops...",
+                                text: result['message'],
+                                confirmButtonColor: '#3A57E8',
+                            });
+                        }
+                    }
+                });
+            } else {
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "You must complete the entire form.",
+                    confirmButtonColor: '#3A57E8',
+                });
+            }
+            return false;
+        });
+    </script>
+@endsection
